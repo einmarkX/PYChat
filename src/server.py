101d4b8cde2543848564a68a875201c6855946f3
1943 <76272409+floppvs@users.noreply.github.com>
2021-07-09 18:55:59 +0700

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,7 +1,26 @@
 #!env python3
 
 
-
+#: PYChat , Server host Script
+#: Copyright Maulana B. (C) 2020-2021
+#:
+#: This Program is free software: you can redistribute it and/or modify
+#: it under terms of the GNU General Public License as published by
+#: Free Software Foundation , either version 3 of the License, or
+#: (at your option) any later version
+#:
+#:
+#: This program is distributed in the hope that it will be useful,
+#: but WITHOUT ANY WARRANTY; without even the implied warranty of
+#: MERCHANTIBILTY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+#: GNU General Public License for more details
+#:
+#: You should have received a copy of the GNU General Public License
+#: along with this program. If not, see <https://www.gnu.org/licenses/>
+#:
+#:
+#:          <Maintained Under>
+#:   Maulana B. <https://github.com/floppvs
 import socket,signal,sys
 
 def handler(signum,frame):
