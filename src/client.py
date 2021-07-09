c4527b908b710b23fc669708c706f186f4f82ee3
1943 <76272409+floppvs@users.noreply.github.com>
2021-07-09 18:55:12 +0700

    Update client.py

--- src/client.py
+++ src/client.py
@@ -1,6 +1,27 @@
 #!env python3
 
 
+#: PYChat Software, Client Script
+#: Copyright Maulana B. (C) 2020-2021
+#:
+#: This Program is free software: you can redistribute it and/or modify
+#: it under terms of the GNU General Public License as published by
+#: Free Software Foundation , either version 3 of the License, or
+#: (at your option) any later version
+#:
+#:
+#: This program is distributed in the hope that it will be useful,
+#: but WITHOUT ANY WARRANTY; without even the implied warranty of
+#: MERCHANTIBILTY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+#: GNU General Public License for more details
+#:
+#: You should have received a copy of the GNU General Public License
+#: along with this program. If not, see <https://www.gnu.org/licenses/>
+#:
+#:
+#:          <Maintained Under>
+#:   Maulana B. <https://github.com/floppvs
+
 import socket
 
 # Before oop
